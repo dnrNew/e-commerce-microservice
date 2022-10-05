b1a81a6ba72a334bb6c230c9b0edda1751c4d45f
Diego Nogueira <dnr.new@gmail.com>
2022-10-05 15:46:39 -0300

    Cria model e mapeia url para o serviço Product
    Cria model para o frontend mapear os atributos do serviço de Produto e
    seta url nas configurações do appsettings para o projeto Web visualizar
    os serviços.

---
+++ eShopMicroservice/eShopMicroservice/eShop.Web/Services/IServices/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Web.Models;
+
+namespace eShop.Web.Services.IServices
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductModel>> GetAllProducts();
+        Task<ProductModel> GetProductById(long id);
+        Task<ProductModel> CreateProduct(ProductModel model);
+        Task<ProductModel> UpdateProduct(ProductModel model);
+        Task<bool> DeleteProductById(long id);
+    }
+}
