e525be2c3d9d0ba6359ba8d4c62df905875a0bf4
Diego Nogueira <dnr.new@gmail.com>
2022-10-05 13:50:23 -0300

    Implementa métodos do Product Repository
    Implementa métodos do Product Repository, todos assíncronos e realizando
    um Crud simples com EF Core.

--- eShopMicroservice/eShopMicroservice/eShop.ProductAPI/Program.cs
+++ eShopMicroservice/eShopMicroservice/eShop.ProductAPI/Program.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using eShop.ProductAPI.Config;
 using eShop.ProductAPI.Model.Context;
+using eShop.ProductAPI.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,6 +14,8 @@ builder.Services.AddDbContext<MySqlContext>(options => options.UseMySql(connecti
 IMapper mapper = MappingConfig.RegisterMaps().CreateMapper();
 builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
