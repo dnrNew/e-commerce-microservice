f0dfbfd4cc55efe34437a183bf6b278f00005d9f
Diego Nogueira <dnr.new@gmail.com>
2022-10-06 20:30:25 -0300

    Cria estrutura inicial do Identity Server
    Cria estrutura inicial do Identity Server utilizando a interface e
    padrões do Duende.

---
+++ eShopMicroservice/eShopMicroservice/eShop.IdentityServer/Model/Context/MySqlContext.cs
@@ -0,0 +1,14 @@
+﻿using eShop.IdentityServer.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eShop.IdentityServer.Model.Context
+{
+    public class MySqlContext : IdentityDbContext<ApplicationUser>
+    {
+        public MySqlContext(DbContextOptions<MySqlContext> options)
+            : base(options)
+        {
+        }
+    }
+}
