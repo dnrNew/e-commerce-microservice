db033f7a283a18c69dc96c9c675ec7f8a9f4e801
Diego Nogueira <dnr.new@gmail.com>
2022-11-09 22:34:38 -0300

    Cria Controller e View para Carrinho de Compras
    Cria Controller e View para o serviço de carrinho de compras, com os
    métodos para trazer os itens do carrinho e remover itens.

---
+++ eShopMicroservice/eShopMicroservice/eShop.Web/Controllers/CartController.cs
@@ -0,0 +1,57 @@
+﻿using eShop.Web.Models;
+using eShop.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eShop.Web.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICartService _cartService;
+
+        public CartController(IProductService productService, ICartService cartService)
+        {
+            _productService = productService;
+            _cartService = cartService;
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CartIndex()
+        {
+            return View(await GetUserCart());
+        }        
+        
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var status = await _cartService.RemoveFromCart(id, token);
+
+            if (status)
+                return RedirectToAction(nameof(CartIndex));
+
+            return View();
+        }
+
+        private async Task<CartViewModel> GetUserCart()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.GetCartByUserId(userId, token);
+
+            if (response?.CartHeader == null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                }
+            }
+
+            return response;
+        }
+    }
+}
